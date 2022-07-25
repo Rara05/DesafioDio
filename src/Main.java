5d28d6b3009b55aa62859e00ad804de1dbaa3375
Rara05 <rayanicristinadelima@gmail.com>
2022-07-24 22:19:04 -0300

    encapsulamento Curso e Mentoria

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //instanciando
+        Curso curso1 = new Curso();
+
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descrição do curso java");
+        curso1.setCargaHoraria(8);
+
+        //instanciando
+        Mentoria mentoria1 = new Mentoria();
+
+        mentoria1.setTitulo("Mentoria Java");
+        mentoria1.setDescricao("Descrição da mentoria");
+        mentoria1.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(mentoria1);
     }
 }
